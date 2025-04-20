7a416a7a48821f8c5fb76a942977650a4d1b96c2
Madhusr116 <madhusudhanraogv@gmail.com>
2025-04-20 11:58:31 +0530

    first commit

--- GCP/cmo/backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "gcs" {
-    bucket  = "terraformstatefilebackend"
-    prefix  = "terraformstatefile"
-credentials = "C:/Users/venkata.m.rao.ganta/GCPkey/cmsr.json"
-  }
-}
--- GCP/genericfile/backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "gcs" {
-    bucket  = "terraformstatefilebackend"
-    prefix  = "terraformstatefile-generic"
-credentials = "C:/Users/venkata.m.rao.ganta/GCPkey/cmsr.json"
-  }
-}
